636bd1515b76cc17a02699876bb7487542212b97
vinzenzzinecker98 <vinzenz@zinecker.de>
2020-07-24 13:57:13 +0200

    Add comments

--- client/src/Validator.js
+++ client/src/Validator.js
@@ -4,8 +4,9 @@ import {Button} from 'react-bootstrap';
 class Validator extends React.Component {
   state = { dataKey1: null };
   result = { result: "u" };
+
+  //When confirmed with Enter (KeyCode 13), Check whether the string matches the SHA-256 pattern, using a Regular Expression, then call the verification method.
   handleKeyDown = e => {
-    // if the enter key is pressed, set the value with the string
     if (e.keyCode === 13) {
       var regex = new RegExp("[A-Fa-f0-9]{64}");
         if(regex.test(e.target.value))
@@ -14,28 +15,23 @@ class Validator extends React.Component {
         }
         else {
           window.confirm("Your given String is not a SHA-256 value, please try again. Please provide the String representation of a SHA256 Hash, which consists of 64 Hex values. Example: a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e")
-        }
-      
+        }      
     }
   };
 
-  
+  //Executes the cachecall on the Documents contract and then saves the datakey to the state `datakey1`
   verification = value => {
     const { drizzle, drizzleState } = this.props;
-    const contract = drizzle.contracts.Documents;
-
-    
+    const contract = drizzle.contracts.Documents;    
     const dataKey1 =  contract.methods["verify"].cacheCall(value);
-
-    console.log(dataKey1);
-
+    //console.log(dataKey1);
     this.setState({dataKey1});
   };
 
-
+  // using the saved `dataKey1`, get the return value of the verify function and display the result. Also provide the "ToClipboard" Button
   getResult = () => {
     const { Documents } = this.props.drizzleState.contracts;
-    // using the saved `dataKey1`, get the return value of GetNumber function
+    
     const result_1 = this.state.dataKey1;
     var address = Documents.verify[this.state.dataKey1];
     //why is it alyws 0 or 1???
@@ -47,21 +43,19 @@ class Validator extends React.Component {
     }
 
     return (
-    <div> This Document <span style={{color: '#a7e362'}}>has been registered</span> by <b>{address && address.value}</b><br></br> <br></br>
-    <Button onCLick={
-      toClip(address.value)
-      } variant="primary">
+    <div> This Document <span style={{color: '#a7e362'}}>has been registered</span> by <b>{address && address.value} </b><br></br><br></br>
+    <Button onCLick={toClip(address.value)} variant="primary">
       <i class="fa fa-clone"></i> 
       &nbsp; Copy the public address to Clipboard
     </Button></div>);
   }
+
   
+  //Renders the Validator Element, including the input Field and the Display for the results of the lookup (Which is empty until a valid input was given).
   render() {  
 
     return (
       <div>
-        
-
         To check whether a document is valid enter its SHA-256 hash value. (Press enter to confirm input) <br></br><br></br>
         <input type="text" size="64" onKeyDown={this.handleKeyDown} />
         
