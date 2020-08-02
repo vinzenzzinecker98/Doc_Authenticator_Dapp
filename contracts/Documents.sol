8767c05cbf093e51bc15130b6e2d9dfbd1b461df
vinzenzzinecker98 <vinzenz@zinecker.de>
2020-08-02 12:44:46 +0200

    add comments

--- contracts/Documents.sol
+++ contracts/Documents.sol
@@ -7,14 +7,15 @@ contract Documents {
   function register (string memory _hash) public
   {
     //Only register document if no one has claimed the exact hash value before.
-    //so that document ownership cannot be altered
+    //document ownership cannot be altered
     if (store[_hash] == address(0)){
-
       //store the owners public adress associated with the hash value.
        store[_hash] = msg.sender;
     }
   }
 
+  //check whether the given value is stored in the ledger
+  //keyword view to enable contract calls only (not altering state)
   function verify (string memory __hash) public view returns (address)
   {
       return store[__hash];
