97ef668890ee03656492b9952ba14392fb8940b3
vinzenzzinecker98 <vinzenz@zinecker.de>
2020-09-18 15:02:02 +0200

    setup testing framework

--- test/Documents.js
+++ test/Documents.js
@@ -1,18 +1,24 @@
-
 const Documents = artifacts.require("./Documents.sol");
 
 
 contract("Documents", accounts => {
-  it("has been stored", async () => {
+  it("should register and validate a document, without changing ownership", async () => {
+
     const documents = await Documents.deployed();
 
     // Register
-    await documents.register("HiMrWorld", { from: accounts[0] });
+    await documents.register("correcthash", { from: accounts[0] });
+
+    //attempt to alter ownership from 0 to 1 
+    await documents.register("correcthash", { from: accounts[1] });
 
     // Validate
-    const a = await documents.verify("HiMrWorldd", {from: accounts[1]});
-    const b = await documents.verify("HiMrWorld", {from: accounts[1]});
-    assert.equal(a, true, "nicht geklappt")
-    
+    const a = await documents.verify("correcthash", {from: accounts[1]});
+    const b = await documents.verify("incorrecthash", {from: accounts[2]});
+   
+
+    assert.equal(a, accounts[0], "registration not working or ownership altered");
+    assert.equal(b, 0x0000000000000000000000000000000000000000, "validation not working, not stored document was said to be registered")
+
   });
 });
